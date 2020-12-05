e344febdab0f29ab981dc8f08054d9a66f48343a
Zzazuu <djxeon@live.se>
2020-12-05 17:45:29 +0100

    Swedish locale

---
+++ locales/sv.lua
@@ -0,0 +1,10 @@
+Locales['sv'] = {
+    ['getting_treatment'] = "Läkaren behandlar dig",
+    ['treatment_done'] = "Behandlingen är färdig, du vart debiterad: ",
+    ['to_many_medics'] = "För många läkare i tjänst",
+    ['only_when_dead'] = "Du kan bara använda detta när du är död",
+    ['not_enough_money'] = "Du har inte råd",
+    ['dispatch_message'] = "En ambulans är påväg mot din destination, håll ut!",
+    ['dispath_company'] = "Pillbox Medical",
+    ['dispatch_request'] = "Läkarvård",
+}
